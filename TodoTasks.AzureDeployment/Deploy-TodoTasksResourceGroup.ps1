6dc6f43df80fd124210b3d21e9914d33db247a99
ste_m <ste_m@DESKTOP-PQF00J8>
2019-07-21 21:13:07 +0200

    Azure arm templates

--- TodoTasks.AzureDeployment/Deploy-TodoTasksResourceGroup.ps1
+++ TodoTasks.AzureDeployment/Deploy-TodoTasksResourceGroup.ps1
@@ -2,8 +2,11 @@ $resourceGroupName = "TodoTasks"
 $location = "NorthEurope"
 
 az group create -n $resourceGroupName -l $location
-az group deployment create --template-file resources\security\roleAssignmentsDeploy.json --parameters resources\security\roleAssignmentsDeploy.parameters.json --resource-group $resourceGroupName
-az group deployment create --template-file resources\web\appServicePlanDeploy.json --parameters resources\web\appServicePlanDeploy.parameters.json --resource-group $resourceGroupName # --query 'properties.outputs.planName.value'
-az group deployment create --template-file resources\web\webAppDeploy.json --parameters resources\web\webAppDeploy.parameters.json --resource-group $resourceGroupName
-az group deployment create --template-file resources\storage\todoDbDeploy.json --parameters resources\storage\todoDbDeploy.parameters.json --resource-group $resourceGroupName 
-az group deployment create --template-file resources\security\keyVaultDeploy.json --parameters resources\security\keyVaultDeploy.parameters.json --resource-group $resourceGroupName
+#az group deployment create --template-file resources\security\roleAssignmentsDeploy.json --parameters resources\security\roleAssignmentsDeploy.parameters.json --resource-group $resourceGroupName
+#az group deployment create --template-file resources\storage\todoDbDeploy.json --parameters resources\storage\todoDbDeploy.parameters.json --resource-group $resourceGroupName 
+#az group deployment create --template-file resources\web\webAppDeploy.json --parameters resources\web\webAppDeploy.parameters.json --resource-group $resourceGroupName
+
+#az group deployment create --template-file resources\web\appServicePlanDeploy.json --parameters resources\web\appServicePlanDeploy.parameters.json --resource-group $resourceGroupName --query 'properties.outputs.planName.value'
+#az sql db show-connection-string -s "todo-db-server-Development" -n "todo-db-Development" -c ado.net --auth-type=SqlPassword
+az group deployment create --template-file TodoTasksDeploy.json --resource-group $resourceGroupName --parameters TodoTasksDeploy.parameters.json
+#az group deployment create --template-file test.json --resource-group $resourceGroupName --parameters test.parameters.json
--- TodoTasks.AzureDeployment/Deploy-TodoTasksSecurityResourceGroup.ps1
+++ TodoTasks.AzureDeployment/Deploy-TodoTasksSecurityResourceGroup.ps1
@@ -1,5 +1,8 @@
 $resourceGroupName = "TodoTasksSecurity"
 $location = "NorthEurope"
 
-$subscription az account list --query [?isDefault].id -o tsv
+#$subscription az account list --query [?isDefault].id -o tsv
 az group create -n $resourceGroupName -l $location
+az group deployment create --template-file resources\security\roleAssignmentsDeploy.json --parameters resources\security\roleAssignmentsDeploy.parameters.json --resource-group $resourceGroupName
+az group deployment create --template-file resources/storage/storageDeploy.json --resource-group $resourceGroupName
+az group deployment create --template-file resources/security/keyVaultDeploy.json --resource-group $resourceGroupName --parameters resources/security/keyVaultDeploy.parameters.json
